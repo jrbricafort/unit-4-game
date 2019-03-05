19a03aebd5bf8102fc64369463ef439f76f6ba12
jrbricafort <josephricafort1@gmail.com>
2019-03-05 13:31:11 -0500

    Mostly complete, just need the crystal values to change upon a win or loss

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,7 @@
 //      * When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
 
-// Chooses a number between 19 - 120.
+// // Chooses a number between 19 - 120.
 var computerChoice = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-console.log(computerChoice);
 
 // variables for wins, losses, and players score
 var wins = 0;
@@ -11,23 +10,15 @@ var playerScore = 0;
 
 
 // This will give each crystal a random hidden value between 1 - 12
-
 var crystalValue = [
     Math.floor(Math.random() * 12) + 1,
     Math.floor(Math.random() * 12) + 1,
     Math.floor(Math.random() * 12) + 1,
-    Math.floor(Math.random() * 12) + 1];
+    Math.floor(Math.random() * 12) + 1]
 
-function initialize() {
-    computerChoice = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-    crystalValue[0] = Math.floor(Math.random() * 12) + 1;
-    crystalValue[1] = Math.floor(Math.random() * 12) + 1;
-    crystalValue[2] = Math.floor(Math.random() * 12) + 1;
-    crystalValue[3] = Math.floor(Math.random() * 12) + 1;
-}
+console.log("Computer wants: " + computerChoice);
+console.log("Crystals are worth: " + crystalValue);
 
-initialize()
-console.log(crystalValue);
 
 ///////////////////         CRYSTAL ONE         ///////////////////
 // Each crystal will be named imageCrystal#
@@ -69,35 +60,44 @@ imageCrystalFour.attr("src", "assets/images/red gem.png");
 imageCrystalFour.attr("data-crystalvalue", crystalValue[3]);
 $("#crystals").append(imageCrystalFour);
 
-
+// This code runs whenever a crystal is clicked
 $(".crystal-image").on("click", function () {
 
-    // Determining the crystal's value requires us to extract the value from the data attribute.
-    // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-    // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-    // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
-
+    // Converts the crystalValue to an integer
     var crystalValue = ($(this).attr("data-crystalvalue"));
     crystalValue = parseInt(crystalValue);
-    // We then add the crystalValue to the user's "counter" which is a global variable.
-    // Every click, from every crystal adds to the global counter.
     playerScore += crystalValue;
     console.log(playerScore);
 
+    // Win condition
     if (playerScore === computerChoice) {
-        alert("You win!");
+        alert("Congratulations! Play again?");
         wins++;
         playerScore = 0;
         computerChoice = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-        crystalValue = Math.floor(Math.random() * 12) + 1;
+        crystalValue = [
+            Math.floor(Math.random() * 12) + 1,
+            Math.floor(Math.random() * 12) + 1,
+            Math.floor(Math.random() * 12) + 1,
+            Math.floor(Math.random() * 12) + 1];
+        console.log("Computer wants: " + computerChoice);
+        console.log("Crystals are worth: " + crystalValue);
+
     }
 
-    else if (playerScore >= computerChoice) {
-        alert("You guessed " + playerScore + ", you were over by " + (playerScore - computerChoice) + ".");
+    // Loss condition
+    else if (playerScore > computerChoice) {
+        alert("You guessed " + playerScore + ", you were over by " + (playerScore - computerChoice) + ". Try again?");
         losses++;
         playerScore = 0;
         computerChoice = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-        crystalValue = Math.floor(Math.random() * 12) + 1;
+        crystalValue = [
+            Math.floor(Math.random() * 12) + 1,
+            Math.floor(Math.random() * 12) + 1,
+            Math.floor(Math.random() * 12) + 1,
+            Math.floor(Math.random() * 12) + 1];
+        console.log("Computer wants: " + computerChoice);
+        console.log("Crystals are worth: " + crystalValue);
     }
 
     var computerChoiceJS = document.getElementById("computerChoiceHTML");
